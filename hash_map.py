084751dc79cecd14ef891e48888a3aead80438d8
S Lucas Myer <50333570+slucasmyer@users.noreply.github.com>
2020-08-26 07:01:59 -0400

    Add files via upload
    Data Structures initial commit

---
+++ hash_map.py
@@ -0,0 +1,179 @@
+# Course: CS261 - Data Structures
+# Assignment: 5
+# Student: Sullivan L Myer
+# Description: Hashtable with constrained data structure and time-complexity
+
+
+# Import pre-written DynamicArray and LinkedList classes
+from a5_include import *
+
+
+def hash_function_1(key: str) -> int:
+    """
+    Sample Hash function #1 to be used with A5 HashMap implementation
+    """
+    hash = 0
+    for letter in key:
+        hash += ord(letter)
+    return hash
+
+
+def hash_function_2(key: str) -> int:
+    """
+    Sample Hash function #2 to be used with A5 HashMap implementation
+    """
+    hash, index = 0, 0
+    index = 0
+    for letter in key:
+        hash += (index + 1) * ord(letter)
+        index += 1
+    return hash
+
+
+class HashMap:
+    def __init__(self, capacity: int, function) -> None:
+        """
+        Init new HashMap based on DA with SLL for collision resolution
+        """
+        self.buckets = DynamicArray()
+        for _ in range(capacity):
+            self.buckets.append(LinkedList())
+        self.capacity = capacity
+        self.hash_function = function
+        self.size = 0
+
+    def __str__(self) -> str:
+        """
+        Return content of hash map t in human-readable form
+        """
+        out = ''
+        for i in range(self.buckets.length()):
+            list = self.buckets.get_at_index(i)
+            out += str(i) + ': ' + str(list) + '\n'
+        return out
+
+    def clear(self) -> None:
+        """
+        clears the current hash table,
+        updating size to 0 but
+        leaving capacity unchanged
+        :return:
+        """
+        self.buckets = DynamicArray()
+        for bucket in range(0, self.capacity):
+            self.buckets.append(LinkedList())
+        self.size = 0
+
+    def get(self, key: str) -> object:
+        """
+        returns either the value associated with the
+        key passed as parameter, or None if key
+        does not currently exist in the hash table
+        :param key:
+        :return: None or value at key
+        """
+        if self.contains_key(key):
+            return self.buckets.get_at_index(self.hash_function(key) % self.capacity).contains(key).value
+        else:
+            return None
+
+    def put(self, key: str, value: object) -> None:
+        """
+        updates the key:value pair in the HashMap,
+        adding a key if one does not exist
+        :param key: key
+        :param value: value
+        :return:
+        """
+
+        if self.contains_key(key):
+            cur_node = self.buckets.get_at_index(self.hash_function(key) % self.capacity).contains(key)
+            cur_node.value = value
+        else:
+            self.size += 1
+            self.buckets.get_at_index(self.hash_function(key) % self.capacity).insert(key, value)
+
+    def remove(self, key: str) -> None:
+        """
+        removes the key passed as parameter,
+        and its associated value from the HashMap.
+        if key is absent, method does not raise exception
+        :param key:
+        :return:
+        """
+        pass
+        if self.contains_key(key):
+            self.buckets.get_at_index(self.hash_function(key) % self.capacity).remove(key)
+            self.size -= 1
+        return
+
+    def contains_key(self, key: str) -> bool:
+        """
+        returns True if the hashmap contains
+        the key passed as parameter, False otherwise
+        :param key:
+        :return:
+        """
+        for bucket in range(0, self.capacity):
+            if self.buckets.get_at_index(bucket).contains(key) is not None:
+                return True
+        return False
+
+    def empty_buckets(self) -> int:
+        """
+        returns number of empty buckets
+        :return: int representing number of empty buckets
+        """
+        empties = 0
+        for bucket in range(0, self.capacity):
+            if self.buckets.get_at_index(bucket).length() == 0:
+                empties += 1
+        return empties
+
+    def table_load(self) -> float:
+        """
+        returns lambda (i.e. nodes divided by buckets)
+        :return: lambda
+        """
+        total_nodes = 0
+        for bucket in range(0, self.buckets.length()):
+            contents = self.buckets.get_at_index(bucket)
+            total_nodes += contents.length()
+        return total_nodes/self.capacity
+
+    def resize_table(self, new_capacity: int) -> None:
+        """
+        changes the capacity of the internal has table,
+        preserving all key value pairs, but rehashing
+        all links, if new_capacity is 0, do nothing
+        :param new_capacity:
+        :return:
+        """
+
+        if new_capacity >= 1:
+            old_size = self.size
+            new_buckets = DynamicArray()
+            for bucket in range(0, new_capacity):
+                new_buckets.append(LinkedList())
+            for bucket in range(0, self.capacity):
+                for node in self.buckets.get_at_index(bucket):
+                    new_buckets.get_at_index(self.hash_function(node.key) % new_capacity).insert(node.key, node.value)
+            self.clear()
+            self.size = old_size
+            self.capacity = new_capacity
+            self.buckets = new_buckets
+            return
+        else:
+            return
+
+    def get_keys(self) -> DynamicArray:
+        """
+        returns a dynamic array that contains all
+        keys stored in the HashMap. order is irrelevant
+        :return: dynamic array containing all unique keys
+        """
+        map_keys = DynamicArray()
+        for bucket in range(0, self.capacity):
+            for node in self.buckets.get_at_index(bucket):
+                map_keys.append(node.key)
+        return map_keys
---
+++ min_heap.py
@@ -0,0 +1,193 @@
+# Course: CS261 - Data Structures
+# Assignment: 5
+# Student: Sullivan Lucas Myer
+# Description: MinHeap implementation with time complexity limitations
+
+
+# Import pre-written DynamicArray and LinkedList classes
+from a5_include import *
+
+
+class MinHeapException(Exception):
+    """
+    Custom exception to be used by MinHeap class
+    """
+    pass
+
+
+class MinHeap:
+    def __init__(self, start_heap=None):
+        """
+        Initializes a new MinHeap
+        """
+        self.heap = DynamicArray()
+
+        # populate MH with initial values (if provided)
+        # before using this feature, implement add() method
+        if start_heap:
+            for node in start_heap:
+                self.add(node)
+
+    def __str__(self) -> str:
+        """
+        Return MH content in human-readable form
+        """
+        return 'HEAP ' + str(self.heap)
+
+    def is_empty(self) -> bool:
+        """
+        Return True if no elements in the heap, False otherwise
+        """
+        return self.heap.length() == 0
+
+    def add(self, node: object) -> None:
+        """
+        adds an element to the heap
+        :param node:
+        :return:
+        """
+        if self.is_empty():
+            self.heap.append(node)
+        else:
+            self.heap.append(node)
+            node_index = self.heap.length() - 1
+            parent_index = (node_index-1)//2
+            while self.heap.get_at_index(parent_index) > node:
+                if parent_index == 0:
+                    self.heap.swap(parent_index, node_index)
+                    break
+                self.heap.swap(parent_index, node_index)
+                node_index = parent_index
+                parent_index = (node_index-1)//2
+
+        return
+
+    def get_min(self) -> object:
+        """
+        returns the min value on the heap
+        :return:
+        """
+        if self.is_empty():
+            raise MinHeapException
+        else:
+            return self.heap.get_at_index(0)
+
+    def remove_min(self) -> object:
+        """
+        removes the min value (root),
+        replacing it while maintaining heap
+        properties
+        :return:
+        """
+        if self.is_empty():
+            raise MinHeapException
+        elif self.heap.length() == 1:
+            return self.heap.pop()
+        else:
+            self.heap.swap(0, (self.heap.length()-1))
+            original_root = self.heap.pop()
+            parent_index = 0
+            l_child_index = 2*parent_index + 1
+            r_child_index = 2*parent_index + 2
+            while l_child_index < self.heap.length() or r_child_index < self.heap.length():
+                if l_child_index < self.heap.length() > r_child_index:
+                    if self.heap.get_at_index(l_child_index) <= self.heap.get_at_index(r_child_index):
+                        if self.heap.get_at_index(parent_index) > self.heap.get_at_index(l_child_index):
+                            self.heap.swap(parent_index, l_child_index)
+                            parent_index = l_child_index
+                            l_child_index = 2 * parent_index + 1
+                            r_child_index = 2 * parent_index + 2
+                        else:
+                            break
+                    else:
+                        if self.heap.get_at_index(parent_index) > self.heap.get_at_index(r_child_index):
+                            self.heap.swap(parent_index, r_child_index)
+                            parent_index = r_child_index
+                            l_child_index = 2 * parent_index + 1
+                            r_child_index = 2 * parent_index + 2
+                        else:
+                            break
+                elif l_child_index < self.heap.length():
+                    if self.heap.get_at_index(parent_index) > self.heap.get_at_index(l_child_index):
+                        self.heap.swap(parent_index, l_child_index)
+                        parent_index = l_child_index
+                        l_child_index = 2 * parent_index + 1
+                        r_child_index = 2 * parent_index + 2
+                    else:
+                        break
+                else:
+                    if self.heap.get_at_index(parent_index) > self.heap.get_at_index(r_child_index):
+                        self.heap.swap(parent_index, r_child_index)
+                        parent_index = r_child_index
+                        l_child_index = 2 * parent_index + 1
+                        r_child_index = 2 * parent_index + 2
+                    else:
+                        break
+            return original_root
+
+    def build_heap(self, da: DynamicArray) -> None:
+        """
+        creates a heap from the unsorted
+        dynamic array passed as parameter
+        :param da:
+        :return:
+        """
+        self.heap = DynamicArray()
+        for i in range(0, da.length()):
+            self.heap.append(da.get_at_index(i))
+        if self.heap.length() <= 1:
+            return
+        else:
+            if ((self.heap.length() - 1) % 2) == 0:  # check if last element is right node
+                r_child_index = self.heap.length() - 1
+                l_child_index = r_child_index - 1
+                parent_index = l_child_index // 2
+
+            else:  # if last element is left node
+                l_child_index = self.heap.length() - 1
+                parent_index = l_child_index // 2
+                if self.heap.get_at_index(parent_index) > self.heap.get_at_index(l_child_index):
+                    self.heap.swap(l_child_index, parent_index)
+                r_child_index = l_child_index - 1
+                l_child_index = r_child_index - 1
+                parent_index = l_child_index // 2
+            while parent_index >= 0:
+                if self.heap.get_at_index(r_child_index) <= self.heap.get_at_index(l_child_index):
+                    child_index = r_child_index
+                else:
+                    child_index = l_child_index
+
+                if self.heap.get_at_index(parent_index) >= self.heap.get_at_index(child_index):
+                    self.heap.swap(child_index, parent_index)
+                    new_p = child_index
+                    new_l = new_p * 2 + 1
+                    new_r = new_p * 2 + 2
+                    ####
+                    if new_r >= self.heap.length():
+                        if new_l < self.heap.length():
+                            if self.heap.get_at_index(new_p) > self.heap.get_at_index(new_l):
+                                self.heap.swap(new_p, new_l)
+                    else:
+                        while (self.heap.get_at_index(new_p) > self.heap.get_at_index(new_l)
+                                or self.heap.get_at_index(new_p) > self.heap.get_at_index(new_r)):
+                            if self.heap.get_at_index(new_l) > self.heap.get_at_index(new_r):
+                                self.heap.swap(new_r, new_p)
+                                new_p = new_r
+                            else:
+                                self.heap.swap(new_l, new_p)
+                                new_p = new_l
+                            new_l = new_p * 2 + 1
+                            new_r = new_p * 2 + 2
+                            if new_r >= self.heap.length():
+                                if new_l < self.heap.length():
+                                    if self.heap.get_at_index(new_p) > self.heap.get_at_index(new_l):
+                                        self.heap.swap(new_p, new_l)
+                                        break
+                                    else:
+                                        break
+                                else:
+                                    break
+                r_child_index = l_child_index - 1
+                l_child_index = r_child_index - 1
+                parent_index = l_child_index // 2
+            return
